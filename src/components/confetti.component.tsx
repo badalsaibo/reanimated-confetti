5ca5ca54e4c91b064e315343d72baff1c07b2ff6
Badal Saibo <develop.badal@gmail.com>
2024-10-11 15:27:13 +0530

    feat: with reanimated

--- src/components/confetti.component.tsx
+++ src/components/confetti.component.tsx
@@ -1,15 +1,59 @@
-import Animated from 'react-native-reanimated';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import ConfettiIcon from '../svg/confetti.svg';
 
 type ConfettiProps = {
   x: number;
   y: number;
+  xVel: number;
+  yVel: number;
   angle: number;
   size: number;
   color: string;
+  angleVel: number;
 };
 
-const Confetti = ({ x, y, angle, color, size }: ConfettiProps) => {
+const Confetti = ({
+  x,
+  y,
+  angle,
+  color,
+  size,
+  angleVel,
+  xVel,
+  yVel,
+}: ConfettiProps) => {
+  const sharedX = useSharedValue(x);
+  const sharedY = useSharedValue(y);
+  const sharedAngle = useSharedValue(angle);
+
+  // const dt = useDerivedValue(() => clock.value / 1000);
+
+  // const dx = useDerivedValue(() => dt.value * xVel);
+  // const dy = useDerivedValue(() => dt.value * yVel);
+  // const dAngle = useDerivedValue(() => dt.value * angleVel);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    // Simulate confetti falling and bouncing
+    sharedX.value = withTiming(sharedX.value + xVel * 0.01, { duration: 16 });
+
+    sharedY.value = withTiming(sharedY.value + yVel * 0.01, { duration: 16 });
+    sharedAngle.value = withTiming(sharedAngle.value + angleVel * 0.01, {
+      duration: 16,
+    });
+
+    return {
+      transform: [
+        { translateX: sharedX.value },
+        { translateY: sharedY.value },
+        { rotate: `${sharedAngle.value}rad` },
+      ],
+    };
+  });
+
   return (
     <Animated.View
       style={[
@@ -18,13 +62,7 @@ const Confetti = ({ x, y, angle, color, size }: ConfettiProps) => {
           top: 0,
           left: 0,
         },
-        {
-          transform: [
-            { translateX: x },
-            { translateY: y },
-            { rotate: `${angle}rad` },
-          ],
-        },
+        animatedStyle,
       ]}>
       <ConfettiIcon width={size} height={size} color={color} />
     </Animated.View>
--- src/components/confettis.component.tsx
+++ src/components/confettis.component.tsx
@@ -7,15 +7,18 @@ const COLORS = ['#00e4b2', '#09aec5', '#107ed5'];
 const CONFETTI_SIZE = 16;
 
 const createConfetti = () => {
-  const { width: screenWidth, height: screenHeight } = Dimensions.get('screen');
+  const { width: screenWidth } = Dimensions.get('screen');
 
   return [...new Array(NUM_CONFETTI)].map((_, i) => {
     return {
       key: i,
-      x: screenWidth * Math.random(),
-      y: screenHeight * Math.random(),
+      x: screenWidth * 0.5 - CONFETTI_SIZE / 2,
+      y: 0,
       angle: Math.PI * 2 * Math.random(),
       color: COLORS[i % COLORS.length],
+      xVel: Math.random() * 400 - 200,
+      yVel: Math.random() * 150 + 150,
+      angleVel: (Math.random() * 3 - 1.5) * Math.PI,
     };
   });
 };
